e4fe37cebe4d9ec4735641394348392b445f9db1
Stephan White <sgradywhite@yahoo.com>
2016-12-08 17:54:26 +0000

    what's up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  host = 'finalseniorproject-sgradywhite.c9users.io/' # Don't use this literally; use your local dev host instead
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
